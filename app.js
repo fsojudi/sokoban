1c23cb648aab0e57769e12b2232b8f04d5300335
fsojudi@gmail.com <fsojudi@gmail.com>
2022-03-30 23:12:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,93 @@
+
+/*   This is the base file for the Sokoban assignment - include this one in your HTML page, before you add the main script file*/
+
+/*   Enum of CSS Classes for the static elements   */
+var Tiles = {
+  Wall: "tile-wall",
+  Space: "tile-space",
+  Goal: "tile-goal"
+};
+
+/*   Enum of CSS Classes for the moving elements   */
+var Entities = {
+  Character: "entity-player",
+  Block: "entity-block",
+  BlockDone: "entity-block-goal"
+};
+
+/*  Legend
+  W = Wall
+  B = Movable block
+  P = Player starting position
+  G = Goal area for the blocks
+*/
+var tileMap01 = {
+  width: 19,
+  height: 16,
+  mapGrid: [
+      [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [[' '], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [[' '], [' '], [' '], [' '], ['W'], [' '], [' '], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [[' '], [' '], [' '], [' '], ['W'], ['B'], [' '], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [[' '], [' '], ['W'], ['W'], ['W'], [' '], [' '], ['B'], ['W'], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [[' '], [' '], ['W'], [' '], [' '], ['B'], [' '], ['B'], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [['W'], ['W'], ['W'], [' '], ['W'], [' '], ['W'], ['W'], [' '], ['W'], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], ['W']],
+      [['W'], [' '], [' '], [' '], ['W'], [' '], ['W'], ['W'], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], [' '], [' '], ['G'], ['G'], ['W']],
+      [['W'], [' '], ['B'], [' '], [' '], ['B'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], ['G'], ['G'], ['W']],
+      [['W'], ['W'], ['W'], ['W'], ['W'], [' '], ['W'], ['W'], ['W'], [' '], ['W'], ['P'], ['W'], ['W'], [' '], [' '], ['G'], ['G'], ['W']],
+      [[' '], [' '], [' '], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W']],
+      [[' '], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+      [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']]
+  ]
+};
+//--------------------------------------------------------
+
+
+
+
+
+var map01 = tileMap01.mapGrid;
+ CreatMap();
+
+
+function CreatMap(){
+
+  for (var i = 0; i <tileMap01.height; i++){
+     
+    for (var j = 0; j < tileMap01.width; j++){
+          
+        switch (map01[i][j].toString()){
+
+              case "W":  SetId("wall");
+                          break;
+              case "B":  SetId("block");
+                          break;
+              case "P":  SetId("player");
+                          break;
+              case "G":  SetId("goal");
+              
+                          break;
+              case " ":  SetId("space");
+                          break;
+              
+          }
+      }
+  }
+}
+
+
+function SetId(idName){
+  var nodeText, attribute;
+  attribute = document.createAttribute("id");
+  attribute.value = idName;
+
+  nodeText = document.createElement("span");
+  
+  nodeText.setAttributeNode(attribute);
+
+  document.getElementById("panel").appendChild(nodeText);
+}
+
